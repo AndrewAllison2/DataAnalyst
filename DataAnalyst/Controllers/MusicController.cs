4b97370a5e9d38a14d140c19deb84192b243c9da
Andrew Allison <andrewallison5211@gmail.com>
2023-12-15 12:52:31 -0700

    class model

--- DataAnalyst/Controllers/MusicController.cs
+++ DataAnalyst/Controllers/MusicController.cs
@@ -1,14 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
 namespace DataAnalyst.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class MusicController : ControllerBase
 {
-    
+  private readonly MusicService _musicService;
+  private readonly Auth0Provider _auth0Provider;
+
+  public MusicController(MusisService musisService, Auth0Provider auth0Provider)
+  {
+    _musicService = musisService;
+    _auth0Provider = auth0Provider;
+  }
+
+  [Authorize]
+  [HttpPost]
+  public async Task<ActionResult<Music>> CreateMusic([FromBody] Music musicData)
+  {
+    try 
+    {
+    Account userInfo = await _auth0Provider.GetUserInfoAsync
+    }
+    catch (Exception e)
+    {
+      return BadRequest(e.Message);
+    }
+  }
 }
---
+++ DataAnalyst/Models/Music.cs
@@ -0,0 +1,20 @@
+namespace DataAnalyst.Models;
+
+public class Music
+{
+    public int Id { get; set; }
+    public string Artist { get; set; }
+    public string Song { get; set; }
+    public string Album { get; set; }
+    public string Genre { get; set; }
+    public int Energy { get; set; }
+    public string SongKey { get; set; }
+    public string CamelotKey { get; set; }
+    public int Tempo { get; set; }
+    public string Notes { get; set; }
+    public string Vibe { get; set; }
+    public string Type { get; set; }
+    public string WorksWith { get; set; }
+}
+
+
