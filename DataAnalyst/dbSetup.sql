0ed157aac0563e76a9df6219df2fc521e01d7c9e
Andrew Allison <andrewallison5211@gmail.com>
2023-12-15 12:26:17 -0700

    DB SETUP

--- DataAnalyst/dbSetup.sql
+++ DataAnalyst/dbSetup.sql
@@ -1,3 +1,5 @@
+USE DataAnalyst
+
 CREATE TABLE IF NOT EXISTS accounts(
   id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
@@ -6,3 +8,15 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE music(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  performer VARCHAR(255) NOT NULL,
+  song VARCHAR(255) NOT NULL,
+  album VARCHAR(255),
+  spotify_genre ENUM('Rock', 'Country', 'Pop', 'Electronic', 'Alternative'),
+  spotify_track_preview_url VARCHAR(500),
+  spotify_track_duration INT NOT NULL,
+  spotify_track_popularity INT NOT NULL,
+  spotify_track_explicit BOOLEAN DEFAULT false
+  ) default charset utf8 COMMENT '';
