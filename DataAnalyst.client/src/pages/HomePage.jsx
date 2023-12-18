91036303b87ba793c6db9f07e839d5e7f4e25ca9
Andrew Allison <andrewallison5211@gmail.com>
2023-12-18 11:17:44 -0700

    launch settings updated, connection active

--- DataAnalyst.client/src/pages/HomePage.jsx
+++ DataAnalyst.client/src/pages/HomePage.jsx
@@ -1,23 +1,19 @@
-import React, { useState } from "react";
+import React from 'react'
 
-export default function HomePage() {
-  const [count, setCount] = useState(0)
-
+const HomePage = () => {
   return (
-    <div className="home-page">
-      <div className="container my-3">
-        <div className="row">
-          <div className="col-4">
-            <div className="card">
-              <div className="card-body">
-                <button className="btn btn-success my-1" onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-                </button>
-              </div>
-            </div>
-          </div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 m-auto">
+          <h1>Wlecome to Music Hub</h1>
         </div>
+
       </div>
+
+
+
     </div>
   )
 }
+
+export default HomePage
